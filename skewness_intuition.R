4176628876116dd6fe71dfa2b3500b88d6087749
dolorez <szymon.adamala@gmail.com>
2020-07-01 18:29:15 +0200

    new question added; refactored question Rmd

--- skewness_intuition.R
+++ skewness_intuition.R
@@ -1,6 +1,8 @@
 library("exams")
 
-skewness_exercises <- c("questions/skewness_intuition.Rmd")
+exams_skeleton(markup = "markdown", encoding = "UTF-8", writer = "exams2pdf")
+
+skewness_exercises <- c("questions/TexasMedianSalePrice.Rmd", "questions/USARape.Rmd")
 ## note that the currency exercise is in UTF-8 encoding
 
 ## generate a single PDF exam (shown in PDF viewer)
@@ -21,12 +23,12 @@ exams2pdf(skewness_exercises
 # 
 # ## generate three PDF exams and corresponding solutions in output directory
 # ## (with the header used to set a custom Date and ID for the exam)
-# exams2pdf(myexam, n = 3, name = c("pdf-exam", "pdf-solution"),
-#           encoding = "UTF-8",
-#           dir = "output",
-#           edir = "exercises",
-#           template = c("templates/exam.tex", "templates/solution.tex"),
-#           header = list(
-#             Date = "2015-01-01",
-#             ID = function(i) formatC(i, width = 5, flag = "0")
-#           ))
+exams2pdf(skewness_exercises, n = 1, name = c("pdf-exercises", "pdf-solution"),
+          encoding = "UTF-8",
+          dir = "output",
+          edir = "exercises",
+          template = c("templates/exam.tex", "templates/solution.tex"),
+          header = list(
+            Date = "2015-01-01",
+            ID = function(i) formatC(i, width = 5, flag = "0")
+          ))
